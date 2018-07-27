05bb5b41996931a1ea3f8822a1ab7d66a028500b
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-07-27 15:01:42 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class Natural
+{
+Public Static void main(String args[])
+{
+int a=0;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<=n;i++)
+{
+a=a+i;
+}
+System.out.println(a);
+}
+}
