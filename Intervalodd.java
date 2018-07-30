e0beaa60102fa3af00c742b59bd434554ebdce3c
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-07-30 16:51:50 +0530

    Update Intervalodd.java

--- Intervalodd.java
+++ Intervalodd.java
@@ -1,13 +1,10 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
- 
-/* Name of the class has to be "Main" only if the class is public. */
 class Intervalodd
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner s=new Scanner(System.in);
 		int m=s.nextInt();
 		int n=s.nextInt();
