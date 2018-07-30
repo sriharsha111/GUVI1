d4e924c2bd2a1b20175d68c14df2c431661c9ec5
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-07-30 16:08:57 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class ultiple
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int c=1;c<=5;c++)
+		{
+			int a=n*c;
+			System.out.println(a);
+		}
+	}
+}
