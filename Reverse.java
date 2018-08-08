9fc07b656867a5f25bb4a747eb46ca1eb3f4a8d5
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-08 12:10:13 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class Reverse
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in)
+char[] a=s.nextLine().toCharArray();
+for(int i=a.length-1;i>=0;i--)
+{
+System.out.print(a[i]);
+}
+System.out.print("\n");
+}
+}
