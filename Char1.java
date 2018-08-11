8e1065d95b88ae0e7d0f37c6704b73709ad42bf5
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-11 12:42:31 +0530

    Create Char1.java

---
+++ Char1.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Char1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String n=s.next();
+		for(char c:n.toCharArray())
+		{
+			if(Character.isDigit(c))
+			{
+				System.out.println("yes");
+				break;
+			}
+			else
+			{
+				System.out.println("no");
+				break;
+			}
+		}
+	}
+}
