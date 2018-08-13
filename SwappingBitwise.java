ea8f2bc19c32c6e1063420fba9a54c75cd018df8
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-13 19:48:43 +0530

    Create SwappingBitwise.java

---
+++ SwappingBitwise.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SwappingBitwise
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		if((a>=b)|(a<=b)|(a==b))
+		{
+		int temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+		}
+		System.out.println(a+" "+b);
+	}
+}
