169af37220b0c01b299e953834de0870f7e2b947
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-13 19:44:18 +0530

    Create Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Swaping
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+	}
+}
