ed78ef3262b4b01c00b226b857236dc6309cff1e
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-13 12:22:06 +0530

    Create Characters.java

---
+++ Characters.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Characters
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				continue;
+ 
+			}
+			count++;
+		}
+ 		System.out.println(count);
+	}
+}
