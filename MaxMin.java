f28d453aef010ad08ea3e7f62e663c33ddd6a9b0
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-14 21:22:57 +0530

    Create MaxMin.java

---
+++ MaxMin.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class MaxMin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		/* package whatever; // don't place package name! */
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		 a[i]=sc.nextInt();
+		 int large=a[0],small=a[0];
+		 for(int i=1;i<n;i++)
+		 {
+		 	if(a[i]>large)
+		 	  large=a[i];
+		 	 if(a[i]<small)
+		 	  small=a[i];
+		 }
+		 System.out.print(small+" "+large);
+	}
+}
