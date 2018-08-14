cc209e7e5560321650c3211c0c3af45a6ce1c899
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-14 20:52:21 +0530

    Update and rename Concat.java to Concatination.java

---
+++ Concatination.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Concatination
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		String s2=s.next();
+		String s3=s1+s2;
+		System.out.println(s3);
+	}
+}
