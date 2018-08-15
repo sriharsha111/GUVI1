d0b55511f601fb2208c9b11d6aeda7e2938019c4
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-15 11:15:18 +0530

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Pow
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     while(a!=1){
+         if(a%2!=0){
+             f=1;
+         }
+         a=a/2;
+        }
+     if(f==1){
+         System.out.println("No");
+     }
+ else{
+         System.out.println("Yes");
+ }
+
+	}
+}
