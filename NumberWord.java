41dfbbca132dab91834bb6df6945411c95009362
sriharsha111 <41734407+sriharsha111@users.noreply.github.com>
2018-08-16 11:04:52 +0530

    Update NumberWord.java

--- NumberWord.java
+++ NumberWord.java
@@ -14,17 +14,17 @@ class NumberWord
 		int n=s.nextInt();
 		switch(n)
 		{
-			case 1:System.out.println("one");break;
-			case 2:System.out.println("two");break;
-			case 3:System.out.println("three");break;
-			case 4:System.out.println("four");break;
-			case 5:System.out.println("five");break;
-			case 6:System.out.println("six");break;
-			case 7:System.out.println("seven");break;
-			case 8:System.out.println("eight");break;
-			case 9:System.out.println("nine");break;
-			case 10:System.out.println("ten");break;
-			default:System.out.println("invalid");
+			case 1:System.out.println("One");break;
+			case 2:System.out.println("Two");break;
+			case 3:System.out.println("Three");break;
+			case 4:System.out.println("Four");break;
+			case 5:System.out.println("Five");break;
+			case 6:System.out.println("Six");break;
+			case 7:System.out.println("Seven");break;
+			case 8:System.out.println("Eight");break;
+			case 9:System.out.println("Nine");break;
+			case 10:System.out.println("Ten");break;
+			default:System.out.println("Invalid");
 		}
 	}
 }
